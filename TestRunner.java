ff84f9de5fd8ef1dc1f99440f002e356af37a39e
Prince Gound <Princestudy.7498@gmail.com>
2026-09-06 21:28:14 +0530

    Test Runner

---
+++ TestRunner.java
@@ -0,0 +1,5 @@
+public class TestRunner{
+    public static void main(String[] args) {
+        System.out.println("Hello world This is only for testrunning or testing the code is running or not");
+    }
+}
